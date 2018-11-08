98f1cd7fef7bc358fa67591060b7a8b8d3b1b15f
Chucka, Zachary Tyler <zchucka@aaaa-24920al.gonzaga.edu>
2018-11-08 09:39:09 -0800

    Initial Commit

---
+++ Web Services Fun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Web Services Fun
+//
+//  Created by Chucka, Zachary Tyler on 11/8/18.
+//  Copyright © 2018 Chucka, Zachary Tyler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
